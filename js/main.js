83012356dd2cdf118a8b9df2a8e77426def169a9
Pablo Jurado <pjurado@overnitecbt.com>
2017-08-01 08:14:04 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".main").onepage_scroll({
+    sectionContainer: "section",
+    responsiveFallback: 600,
+    loop: false // this is to go back to the top
+  });
+});
