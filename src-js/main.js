6ae944f1ccefd93b8a7f205717358177e9cd0214
Pablo Jurado <pjurado85@gmail.com>
2017-09-12 19:02:09 -0500

    testing full screen on mobile

--- src-js/main.js
+++ src-js/main.js
@@ -3,8 +3,7 @@
 // main scroll setUp
 $('.main').onepage_scroll({
   sectionContainer: 'section',    // sectionContainer accepts any kind of selector in case you don't want to use section
-  easing: 'ease',                 // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-                                  // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+  easing: 'ease',                 // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
   animationTime: 600,             // AnimationTime let you define how long each section takes to animate
   pagination: true,               // You can either show or hide the pagination. Toggle true for show, false for hide.
   updateURL: false,               // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
@@ -33,6 +32,18 @@ var isImageShowing = false
 var isAboutShowing = false
 var isHeaderShowing = false
 var portfolioIndex = 0
+
+// check for phone fullscreen
+var body = document.documentElement;
+if (body.requestFullscreen) {
+  body.requestFullscreen();
+} else if (body.webkitrequestFullscreen) {
+  body.webkitrequestFullscreen();
+} else if (body.mozrequestFullscreen) {
+  body.mozrequestFullscreen();
+} else if (body.msrequestFullscreen) {
+  body.msrequestFullscreen();
+}
 
 function animateContactMe () {
   $('.contact-me').velocity('transition.fadeIn', animateLinks)
